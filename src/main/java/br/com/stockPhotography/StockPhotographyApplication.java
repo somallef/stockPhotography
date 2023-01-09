a12fdaacc0f5075adb244b25af03870111991dec
Allef Mizael <allef.mizael@gmail.com>
2023-01-09 19:41:32 -0300

    primeiro commit

---
+++ src/main/java/br/com/stockPhotography/StockPhotographyApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockPhotography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPhotographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPhotographyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stockPhotography/StockPhotographyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockPhotography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPhotographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
